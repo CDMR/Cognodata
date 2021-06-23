4c83698f31de3488b1b6d9f021a85023522916b0
CDMR <carlosd.marceleno@gmail.com>
2021-06-23 16:04:28 -0500

    carreccion1

--- Ejercicio1.2.py
+++ Ejercicio1.2.py
@@ -3,14 +3,13 @@ import pandas as pd
 matriz=[]
 filas = 10
 columnas= 7   
-for i in range(filas):
-    for j in range(columnas):
-        matriz.append([0]*columnas)
     
 for i in range(filas):
+    matriz.append([0]*columnas)
     for j in range(columnas):
         matriz[i][j]= round(random.uniform(0,100),2)
 
 dias=['lunes','martes','miercoles','jueves','viernes','sabado','domingo']
 df= pd.DataFrame(data=matriz, columns=dias)
-df
+print(matriz)
+print(df)
